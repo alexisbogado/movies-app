8efd4a2cd474b00f296c805534682b53d041ab3c
Alexis Bogado <alexis.bogado@icloud.com>
2021-12-20 20:45:58 +0100

    Moved mobile page title style to a separate file

---
+++ src/pages/discover/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import { mobileBreakpoint, contentPaddingX } from 'theme/sizes'
+
+export const MobilePageTitle = styled.header`
+  display: flex;
+  align-items: center;
+  gap: 30px;
+  font-size: 2em;
+  line-height: 0;
+  padding: 20px ${contentPaddingX}px;
+
+  @media (min-width: ${mobileBreakpoint}px) {
+    display: none;
+  }
+`
