65f58bab554bfa622166cfba19562aed0596167b
Alexis Bogado <alexis.bogado@icloud.com>
2021-12-20 19:33:51 +0100

    Replace useNavBarToggler hook to useIsOpen

--- src/contexts/mainContext.js
+++ src/contexts/mainContext.js
@@ -1,5 +1,6 @@
 import { createContext } from 'react'
-import useNavBarToggler from 'hooks/useNavBarToggler'
+import { mobileBreakpoint } from 'theme/sizes'
+import useIsOpen from 'hooks/useIsOpen'
 
 const MainContext = createContext({
   isOpen: false,
@@ -7,8 +8,10 @@ const MainContext = createContext({
   close: () => { },
 })
 
+const isNavBarOpened = window.innerWidth > mobileBreakpoint
+
 const MainContextProvider = ({ children }) => {
-  const { isOpen, open, close } = useNavBarToggler()
+  const { isOpen, open, close } = useIsOpen(isNavBarOpened)
 
   return (
     <MainContext.Provider value={{ isOpen, open, close }}>
