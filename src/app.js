ee823a9311eb4d85e781b8cbaa657542e6568f2e
Alexis Bogado <alexis.bogado@icloud.com>
2021-12-20 16:22:15 +0100

    Move styled components to a separate file Add main context provider

--- src/app.js
+++ src/app.js
@@ -1,35 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import styled from 'styled-components';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { PageContainer, ContentWrapper } from './app.styles'
+import { MainContextProvider } from './contexts/mainContext'
+import SideNavBar from './components/sidenavbar'
+import Discover from './pages/discover'
 
-import SideNavBar from "./components/sidenavbar";
+import './css/app.css'
 
-import Discover from "./pages/discover";
+export default function App() {
+  return (
+    <Router>
+      <PageContainer>
+        <MainContextProvider>
+          <SideNavBar />
 
-import './css/app.css'; 
-
-export default class App extends React.Component {
-  render () {
-    return (
-      <Router>
-        <PageContainer>
-          <SideNavBar {...this.props} />
           <ContentWrapper>
             <Switch>
-              <Route path="/discover" component={Discover} {...this.props}/>
+              <Route path="/discover" component={Discover} />
             </Switch>
           </ContentWrapper>
-        </PageContainer>
-      </Router>
-    );
-  }
+        </MainContextProvider>
+      </PageContainer>
+    </Router>
+  )
 }
-
-
-const ContentWrapper = styled.main`
-  padding-left: 280px;
-`
-
-const PageContainer = styled.main`
-  overflow-x: hidden;
-`
