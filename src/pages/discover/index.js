b7796db0771a5c581ba345d8cb2fa8534e5b34d5
Alexis Bogado <alexis.bogado@icloud.com>
2021-12-21 16:42:19 +0100

    Implement search filters Removed useless commented lines

--- src/pages/discover/index.js
+++ src/pages/discover/index.js
@@ -2,49 +2,48 @@ import {
   Wrapper,
   TotalCounter,
   MovieResults,
+  MovieFilters,
 } from './styles'
 import MovieList from 'components/movielist'
+import SearchFilters from 'components/searchfilters'
 import useGenres from 'hooks/useGenres'
 import useMovies from 'hooks/useMovies'
 
-// import SearchFilters from '../../components/searchfilter'
-
 export default function Discover() {
-  const { getGenreById } = useGenres()
+  const { genres, getGenreById } = useGenres()
   const { result, total } = useMovies()
 
-  // const state = {
-  //   keyword: '',
-  //   year: 0,
-  //   genreOptions: [],
-  //   ratingOptions: [
-  //     { id: 7.5, name: 7.5 },
-  //     { id: 8, name: 8 },
-  //     { id: 8.5, name: 8.5 },
-  //     { id: 9, name: 9 },
-  //     { id: 9.5, name: 9.5 },
-  //     { id: 10, name: 10 }
-  //   ],
-  //   languageOptions: [
-  //     { id: 'GR', name: 'Greek' },
-  //     { id: 'EN', name: 'English' },
-  //     { id: 'RU', name: 'Russian' },
-  //     { id: 'PO', name: 'Polish' }
-  //   ]
-  // }
+  const ratingOptions = [
+    { id: 7.5, name: 7.5 },
+    { id: 8, name: 8 },
+    { id: 8.5, name: 8.5 },
+    { id: 9, name: 9 },
+    { id: 9.5, name: 9.5 },
+    { id: 10, name: 10 }
+  ]
+
+  const languageOptions = [
+    { id: 'GR', name: 'Greek' },
+    { id: 'EN', name: 'English' },
+    { id: 'RU', name: 'Russian' },
+    { id: 'PO', name: 'Polish' }
+  ]
 
   // Write a function to trigger the API request and load the search results based on the keyword and year given as parameters
+  const searchMovies = (keyword, year) => {
+    throw new Error('To be implemented')
+  }
 
   return (
     <Wrapper>
-      {/* <MovieFilters>
+      <MovieFilters>
         <SearchFilters
-          genres={genreOptions} 
+          genres={genres} 
           ratings={ratingOptions}  
           languages={languageOptions}
-          searchMovies={(keyword, year) => this.searchMovies(keyword, year)}
+          searchMovies={(keyword, year) => searchMovies(keyword, year)}
         />
-      </MovieFilters> */}
+      </MovieFilters>
 
       <MovieResults>
         { total > 0 && <TotalCounter>{total} movies</TotalCounter>}
