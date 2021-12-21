d1927815e69214c8a3f87db33a1051c43075f0c1
Alexis Bogado <alexis.bogado@icloud.com>
2021-12-21 12:50:23 +0100

    Use Suspense to show loader if a route is loading

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
+import { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { PageContainer, ContentWrapper } from './app.styles'
 import { MainContextProvider } from './contexts/mainContext'
 import { routes } from './routes'
 import MobilePageTitle from 'components/mobilepagetitle'
 import SideNavBar from './components/sidenavbar'
+import Loader from 'components/loader'
 
 import './css/app.css'
 
@@ -17,11 +19,13 @@ export default function App() {
           <ContentWrapper>
             <MobilePageTitle />
             
-            <Switch>
-              {routes.map((route, index) => (
-                <Route {...route} key={index} />
-              ))}
-            </Switch>
+            <Suspense fallback={<Loader />}>
+              <Switch>
+                {routes.map((route, index) => (
+                  <Route {...route} key={index} />
+                ))}
+              </Switch>
+            </Suspense>
           </ContentWrapper>
         </MainContextProvider>
       </PageContainer>
