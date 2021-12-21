f1056b5890a98e088cc32c11c756f6f2ce8b3585
Alexis Bogado <alexis.bogado@icloud.com>
2021-12-21 01:41:07 +0100

    Convert to functional component and move styled components into a separate file

--- src/components/movielist/index.js
+++ src/components/movielist/index.js
@@ -1,30 +1,20 @@
 import MovieItem from 'components/movieitem'
-import React from "react";
-import styled from 'styled-components';
+import { Wrapper } from './styles'
 
-// import MovieItem from '../movieitem';
-
-export default class MovieList extends React.Component {
-
-  render () {
-    // const { movies, genres } = this.props;
-    const item = {
-      title: 'Spider-Man: No Way Home',
-      picture: 'https://www.themoviedb.org/t/p/w220_and_h330_face/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg',
-      score: 8.7,
-      category: 'Action | Adventure | Science Fiction',
-      description: 'Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.',
-      date: '2021-12-15'
-    }
-
-    return (
-      <MoviesWrapper>
-        <MovieItem {...item} />
-      </MoviesWrapper>
-    )
+export default function MovieList({ movies, genres }) {
+  const item = {
+    title: 'Spider-Man: No Way Home',
+    picture: 'https://www.themoviedb.org/t/p/w220_and_h330_face/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg',
+    score: 8.7,
+    category: 'Action | Adventure | Science Fiction',
+    description: 'Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.',
+    date: '2021-12-15'
   }
-}
 
-const MoviesWrapper = styled.div`
-  position: relative;
-`
+  return (
+    <Wrapper>
+      <MovieItem {...item} />
+      <MovieItem {...item} />
+    </Wrapper>
+  )
+}
