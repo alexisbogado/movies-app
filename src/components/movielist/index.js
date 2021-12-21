057b8f9235e26399f5611943f63845173bb16488
Alexis Bogado <alexis.bogado@icloud.com>
2021-12-21 01:32:43 +0100

    Add default movie to render

--- src/components/movielist/index.js
+++ src/components/movielist/index.js
@@ -1,3 +1,4 @@
+import MovieItem from 'components/movieitem'
 import React from "react";
 import styled from 'styled-components';
 
@@ -7,10 +8,18 @@ export default class MovieList extends React.Component {
 
   render () {
     // const { movies, genres } = this.props;
+    const item = {
+      title: 'Spider-Man: No Way Home',
+      picture: 'https://www.themoviedb.org/t/p/w220_and_h330_face/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg',
+      score: 8.7,
+      category: 'Action | Adventure | Science Fiction',
+      description: 'Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.',
+      date: '2021-12-15'
+    }
 
     return (
       <MoviesWrapper>
-        {/* Finish the MovieItem component and use it here to display the movie results */}
+        <MovieItem {...item} />
       </MoviesWrapper>
     )
   }
