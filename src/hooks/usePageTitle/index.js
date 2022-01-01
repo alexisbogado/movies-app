24ce844bacc6374dce38052e5f6f117741adc99f
Alexis Bogado <alexis.bogado@icloud.com>
2022-01-01 23:45:07 +0100

    Moved usePageTitle hook to a folder

--- src/hooks/usePageTitle/index.js
+++ src/hooks/usePageTitle/index.js
@@ -2,9 +2,10 @@ import { useEffect, useState } from 'react'
 
 export default function usePageTitle(initialTitle) {
   const [ title, setTitle ] = useState(initialTitle)
+  const { REACT_APP_TITLE } = process.env
 
   useEffect(() => {
-    document.title = `Movies App - ${title}`
+    document.title = `${REACT_APP_TITLE} - ${title}`
   }, [ title ])
 
   return { title, setTitle }
