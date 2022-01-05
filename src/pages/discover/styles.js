851f09dac0fb46e66cac03c555cef64dc653178f
Alexis Bogado <alexis.bogado@icloud.com>
2022-01-05 15:49:25 +0100

    Add gap just if user is from desktop

--- src/pages/discover/styles.js
+++ src/pages/discover/styles.js
@@ -7,10 +7,10 @@ export const Wrapper = styled.main`
   grid-column: 2;
   grid-row: 1;
   grid-auto-flow: row;
-  gap: 15px;
   grid-template-columns: 1fr auto;
 
   @media (min-width: ${desktopBreakpoint}px) {
+    gap: 15px;
     padding: 60px ${contentPaddingX}px;
     grid-auto-flow: column;
   }
